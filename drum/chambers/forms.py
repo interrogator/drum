78eb898d6b5972b99f6c11e9c00139fbec7df211
Danny McDonald <daniel.mcdonald@bitpanda.com>
2019-03-19 19:14:21 +0100

    add automod form and columns

--- drum/chambers/forms.py
+++ drum/chambers/forms.py
@@ -1,11 +1,21 @@
 from django.forms.models import modelform_factory
 from django.forms import ValidationError
-
+from django import forms
+from django.conf import settings
 from drum.chambers.models import Chamber
 
+AUTOMOD_CHOICES = [(None, 'None')] + [(k, v) for k, v in settings.EVALUATORS.items()]
 
-fields = ["display_name", "description", "automod"]
-BaseChamberForm = modelform_factory(Chamber, fields=fields)
+mod_and_sev = list()
+mods = list()
+for char in 'abcde':
+    mods.append('automod_{}'.format(char))
+    mod_and_sev.append('automod_{}'.format(char))
+    mod_and_sev.append('severity_{}'.format(char))
+
+fields = ["chamber", "description"] + mod_and_sev
+widgets = {name: forms.Select(choices=AUTOMOD_CHOICES) for name in mods}
+BaseChamberForm = modelform_factory(Chamber, fields=fields, widgets=widgets)
 
 
 class ChamberForm(BaseChamberForm):
--- drum/chambers/models.py
+++ drum/chambers/models.py
@@ -8,20 +8,51 @@ from mezzanine.core.models import Displayable, Ownable
 from mezzanine.core.request import current_request
 from mezzanine.generic.fields import RatingField, CommentsField
 
+from decimal import Decimal
 
 USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
+
+AUTOMOD = dict(blank=True,
+               max_length=100)
+
+SEVERITY = dict(max_digits=3,
+                decimal_places=2,
+                default=Decimal('0'))
 
 
 class Chamber(Displayable, Ownable):
 
-    display_name = models.CharField(max_length=200)
+    chamber = models.CharField(max_length=200)
     rating = RatingField()
     comments = CommentsField()
-    automod = models.CharField(max_length=100)  # dummy for now
+
+    # ugly automod stuff...
+    automod_a = models.CharField(**AUTOMOD)
+    automod_b = models.CharField(**AUTOMOD)
+    automod_c = models.CharField(**AUTOMOD)
+    automod_d = models.CharField(**AUTOMOD)
+    automod_e = models.CharField(**AUTOMOD)
+    severity_a = models.DecimalField(**SEVERITY)
+    severity_b = models.DecimalField(**SEVERITY)
+    severity_c = models.DecimalField(**SEVERITY)
+    severity_d = models.DecimalField(**SEVERITY)
+    severity_e = models.DecimalField(**SEVERITY)
+
+    def _automod_config(self):
+        """
+        Helper to get/format automod config for this chamber
+        """
+        out = dict()
+        for char in 'abcde':
+            mod = getattr(self, 'automod_{}'.format(char))
+            sev = getattr(self, 'severity_{}'.format(char))
+            if mod:
+                out[mod] = float(sev)
+        return out
 
     def get_absolute_url(self):
-        kwa = {"display_name": self.display_name}
-        return reverse("chamber_detail", kwargs=kwa)
+        kwa = {"chamber": self.chamber}
+        return reverse("chamber_view", kwargs=kwa)
 
     @property
     def domain(self):
